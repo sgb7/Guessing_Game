6c550ec35ab883af34df8fb219bf961ac4b81b8d
Summer <sgbanister@gmail.com>
2020-03-12 17:30:43 -0700

    Added beginning attempt

---
+++ Animal_Guessing_Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Node
+{
+    public string data;
+    public Node left, right;
+
+    public Node(string info)
+    {
+        data = info;
+        left = right = null;
+    }
+}
+
+public class BinaryTree
+{
+    Node root;
+
+    BinaryTree(string info)
+    {
+        root = new Node(info);
+    }
+
+    BinaryTree()
+    {
+        root = null;
+    }
+}

a564bc1a19b07e3d5948e4f584d856c2d53d380b
Summer <sgbanister@gmail.com>
2020-03-29 20:47:53 -0700

    Edited project

---
+++ GuessingGame/GuessingGame/BinaryTree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace BinaryTreeProject
+{
+    public class Node
+    {
+        public string data;
+        public Node leftChild;
+        public Node rightChild;
+
+        public Node(string value)
+        {
+            data = value;
+            leftChild = null;
+            rightChild = null;
+        }
+
+        public bool isLeaf(ref Node node)
+        {
+            return (node.leftChild == null && node.rightChild == null);
+        }
+
+        public void insertData(ref Node node, string input, string input2)
+        {
+            
+            if (node == null)
+            {
+                node = new Node(input);
+            }
+            else
+            {
+                if (input2 == "no")
+                {
+                    insertData(ref node.leftChild, input, input2);
+                }
+                else if (input2 == "yes")
+                {
+                    insertData(ref node.rightChild, input, input2);
+                }
+                else
+                {
+                    Console.WriteLine("This is an invalid response. Please answer yes or no.");
+                }
+            }
+        }
+
+        public void display(Node n)
+        {
+            if (n == null)
+            {
+                return;
+            }
+
+            display(n.leftChild);
+            Console.Write(" " + n.data);
+            display(n.rightChild);
+        }
+    }
+
+    public class BinaryTree
+    {
+        private Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public bool isEmpty()
+        {
+            return root == null;
+        }
+
+        public void insert(string input, string input2)
+        {
+            if (isEmpty())
+            {
+                root = new Node(input);
+            }
+            else
+            {
+                root.insertData(ref root, input, input2);
+            }
+        }
+
+        public bool isLeaf()
+        {
+            if (!isEmpty())
+            {
+                return root.isLeaf(ref root);
+            }
+
+            return true;
+        }
+
+        public void display()
+        {
+            if (!isEmpty())
+            {
+                root.display(root);
+            }
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BinaryTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Animal Guessing Game\n");
+
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.insert("Is it a cat?", "");
+            binaryTree.display();
+
+            string correctOrIncorrect;
+            string actualAnimal;
+            string newQuestion;
+            string yesOrNo;
+            string continuePlaying = "";
+
+            while(continuePlaying != "no")
+            {
+                //print root
+                correctOrIncorrect = Console.ReadLine();
+                if (correctOrIncorrect == "Yes")
+                {
+                    Console.WriteLine("Hooray! I win! :)");
+                }
+                else
+                {
+                    Console.WriteLine("What animal were you thinking of then?");
+                    actualAnimal = Console.ReadLine();
+
+                    Console.WriteLine("Could you provide a yes or no question to help differentiate this animal?");
+                    newQuestion = Console.ReadLine();
+
+                    Console.WriteLine("Is the answer to this question yes or no?");
+                    yesOrNo = Console.ReadLine();
+
+                    Console.WriteLine("Would you like to play again?");
+                    continuePlaying = Console.ReadLine();
+                }
+            }
+
+            /*binaryTree.insert("Hello");
+            binaryTree.insert("This");
+            binaryTree.insert("Is");
+            binaryTree.insert("A");
+            binaryTree.insert("Test.");
+            //binaryTree.display();*/
+        }
+    }
+}
